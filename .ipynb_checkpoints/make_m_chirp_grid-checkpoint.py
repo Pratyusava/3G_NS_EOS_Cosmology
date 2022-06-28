7d6aabd43e6f9081385466fa00cc993fdfd2e1ea
Pratyusava Baral <pratyusava.baral@ldas-pcdev1.ligo.caltech.edu>
2022-06-28 15:11:55 -0700

    No bias if mass priors are removed

--- .ipynb_checkpoints/make_m_chirp_grid-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from basic_functions import *
-l = 150
-log_q_array = np.linspace(np.log(0.3),np.log(1),l)
-log_lambda_tilde_array = np.linspace(4,10,l)
-m_chirp_grid = m_chirp_grid_from_log_q_log_lambda_tilde(log_q_array,log_lambda_tilde_array)
-np.savetxt('m_chirp_grid_using_SLY'+str(l)+'.txt', m_chirp_grid)
--- Posterior_on_H0.py
+++ Posterior_on_H0.py
@@ -2,17 +2,17 @@ import h5py
 import numpy as np
 from basic_functions import *
 
-FNAME = 'data_seed_70.h5'
+FNAME = '../Data/optimal/data_seed_70.h5'
 f = h5py.File(FNAME, "r")
 from scipy.stats import norm, beta
 
-n_events = 10000#00#00#0
+n_events = 100000#00#00#0
 chirp_mass_prior_bounds = np.array([1,1.4])
 log_q_prior_bounds = np.log(np.array([0.7, 0.95]))
 z_by_10_prior_pdf = beta(3,9)
 z_array = np.linspace(0.0001,10.,2000)
 effective_distance_population = f['effective distance population'][:]
-H0 = np.linspace(70.4, 70.8, 11)
+H0 = np.linspace(70.4, 70.6, 11)
 log_likelihood_all_events = np.zeros(len(H0))
 for n_events_ in range(n_events):
     log_likelihood = []
@@ -24,16 +24,15 @@ for n_events_ in range(n_events):
         mc_samples = f['m_chirp_samples'+str(n_events_)][:]
         log_q_samples = f['log_q_samples'+str(n_events_)][:]
         prior_z_samples = z_by_10_prior_pdf.pdf(z_samples/10)
-        mask = mc_samples < chirp_mass_prior_bounds[0]
+        mask = mc_samples < 0#chirp_mass_prior_bounds[0]
         prior_z_samples[mask] = 0
-        mask = mc_samples > chirp_mass_prior_bounds[1] 
-        prior_z_samples[mask] = 0
-        mask = (log_q_samples >  log_q_prior_bounds[1]) #& (log_q_samples <  -log_q_prior_bounds[1])
-        prior_z_samples[mask] = 0
-        mask = (log_q_samples < log_q_prior_bounds[0]) #& (log_q_samples > -log_q_prior_bounds[0])
-        prior_z_samples[mask] = 0
-        #print (len(np.where(prior_z_samples == 0)[0]))
-        #sys.exit()
+        # mask = mc_samples > chirp_mass_prior_bounds[1] 
+        # prior_z_samples[mask] = 0
+        # mask = (log_q_samples >  log_q_prior_bounds[1]) #& (log_q_samples <  -log_q_prior_bounds[1])
+        # prior_z_samples[mask] = 0
+        # mask = (log_q_samples < log_q_prior_bounds[0]) #& (log_q_samples > -log_q_prior_bounds[0])
+        # prior_z_samples[mask] = 0
+        
         log_D_L_mean = np.log(effective_distance_population[n_events_])
         log_D_L_sigma = np.array(f['log_effective_distance_error'+str(n_events_)])
         log_D_L_samples = np.log(np.interp(z_samples, cosmo_true_z_luminosity_distance_list[0], cosmo_true_z_luminosity_distance_list[1]))#/theta_samples
@@ -50,4 +49,4 @@ plt.plot(H0, np.exp(log_likelihood_all_events-np.max(log_likelihood_all_events))
 plt.xlabel('H0')
 plt.ylabel('Likelihood')
 plt.axvline(70.5, color='r')
-plt.savefig(str(n_events)+'_events.png')
+plt.savefig(str(n_events)+'_events_wo_mass_prior.png')
